769f8dfbc4280db8a7f19ee155b1d695afec1b32
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-09-09 22:38:56 +0530

    video view created

--- app/src/main/java/com/example/hirunews/ui/newsview/newsViewActivity.java
+++ app/src/main/java/com/example/hirunews/ui/newsview/newsViewActivity.java
@@ -2,15 +2,33 @@ package com.example.hirunews.ui.newsview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.hirunews.R;
+import com.example.hirunews.ui.videoview.VideoViewActivity;
+import com.google.android.material.snackbar.Snackbar;
 
 public class newsViewActivity extends AppCompatActivity {
+
+    ImageView playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_view);
+
+        playButton = findViewById(R.id.playBtn);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(newsViewActivity.this, VideoViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
