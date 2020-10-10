3ee4e6c53497fe1d458eae700cb1336d7a0fa5a6
MenuraDewalegama <61499065+MenuraDewalegama@users.noreply.github.com>
2020-10-10 23:32:49 +0530

    Modified other news layouts.

--- app/src/main/java/com/example/hirunews/ui/categories/othernews/CiaNewsFragment.java
+++ app/src/main/java/com/example/hirunews/ui/categories/othernews/CiaNewsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +14,71 @@ import com.example.hirunews.R;
 
 public class CiaNewsFragment extends Fragment {
 
+    LinearLayout cia_news_1, cia_news_2, cia_news_3, cia_news_4, cia_news_5, cia_news_6, cia_news_7;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
 
         View root = inflater.inflate(R.layout.fragment_cia_news, container, false);
+
+        cia_news_1 = root.findViewById(R.id.cia_news_1);
+        cia_news_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cia_news_2 = root.findViewById(R.id.cia_news_2);
+        cia_news_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cia_news_3 = root.findViewById(R.id.cia_news_3);
+        cia_news_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cia_news_4 = root.findViewById(R.id.cia_news_4);
+        cia_news_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cia_news_5 = root.findViewById(R.id.cia_news_5);
+        cia_news_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cia_news_6 = root.findViewById(R.id.cia_news_6);
+        cia_news_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cia_news_7 = root.findViewById(R.id.cia_news_7);
+        cia_news_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
         return root;
     }
--- app/src/main/java/com/example/hirunews/ui/categories/othernews/EntertainmentNewsFragment.java
+++ app/src/main/java/com/example/hirunews/ui/categories/othernews/EntertainmentNewsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +14,72 @@ import com.example.hirunews.R;
 
 public class EntertainmentNewsFragment extends Fragment {
 
+    LinearLayout entertainment_news_1, entertainment_news_2, entertainment_news_3, entertainment_news_4, entertainment_news_5, entertainment_news_6,
+            entertainment_news_7;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
 
         View root = inflater.inflate(R.layout.fragment_entertainment_news, container, false);
+
+        entertainment_news_1 = root.findViewById(R.id.entertainment_news_1);
+        entertainment_news_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        entertainment_news_2 = root.findViewById(R.id.entertainment_news_2);
+        entertainment_news_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        entertainment_news_3 = root.findViewById(R.id.entertainment_news_3);
+        entertainment_news_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        entertainment_news_4 = root.findViewById(R.id.entertainment_news_4);
+        entertainment_news_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        entertainment_news_5 = root.findViewById(R.id.entertainment_news_5);
+        entertainment_news_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        entertainment_news_6 = root.findViewById(R.id.entertainment_news_6);
+        entertainment_news_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        entertainment_news_7 = root.findViewById(R.id.entertainment_news_7);
+        entertainment_news_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
         return root;
     }
